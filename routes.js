b30ad2ba54135164bb5eec8b39733f5b7e36150c
Stéphane <stephane@stephane-VPCYB1S1E.(none)>
2011-12-14 20:55:01 +0100

    less ugly

--- routes.js
+++ routes.js
@@ -21,22 +21,7 @@ module.exports = function(app, db, bcrypt){
 
 
 	app.post('/new_account', function(req, res){
-//		user.new(req, res, db, bcrypt, res.redirect('/setup'));
-	var salt = bcrypt.gen_salt_sync(10), 
-			user = req.body.user.email,
-			pwd = req.body.user.password,
-			hash = bcrypt.encrypt_sync(pwd, salt),
-			random = Math.floor(Math.random()*1000);
-	db.incr('next.user.id', function(err, result){
-		var id_count = result,
-				random_id = id_count + '' + random;
-		db.set(user + ':hash', hash, function(err, result){
-			db.set(user + ':id', random_id, function(err, result){
-				req.session.userid = random_id;
-				res.redirect('/setup');
-			});
-		});	
-	});
+		user.new(req, res, db, bcrypt, res.redirect('/setup'));
 	});
 
 
